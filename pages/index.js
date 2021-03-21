787df9e65cafff759cc7f30108f9952304326e09
Parm <parmjitkunner@gmail.com>
2021-03-21 23:30:32 +0000

    comment added

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export const getStaticProps = async () => {
     "https://jsonplaceholder.typicode.com/posts?_limit=6"
   );
   const articles = await res.json();
-
+  // test
   return {
     props: {
       articles,
